c12717bde8820fbaa2ab6e0e50be32cc712f17d6
x45iq <XRzDckOfjK@proton.me>
2025-11-21 01:52:57 +0200

    update java version extraction

--- hm-module.nix
+++ hm-module.nix
@@ -17,22 +17,6 @@
 
   cfg = config.programs.xmcl;
 
-  getJavaVersion = jre:
-    pkgs.runCommand "get-java-version"
-    {
-      nativeBuildInputs = [jre];
-    }
-    ''
-      java_version=$(${jre}/bin/java -version 2>&1 | head -n1 | cut -d'"' -f2)
-      major_version=$(echo $java_version | cut -d'.' -f1)
-      if [ "$major_version" = "1" ]; then
-        major_version=$(echo $java_version | cut -d'.' -f2)
-      fi
-      mkdir -p $out/
-      echo -n "$java_version" > $out/version
-      echo -n "$major_version" > $out/major
-    '';
-
   createJavaJson = jres:
     if jres == []
     then pkgs.writeText "java.json" (builtins.toJSON {all = [];})
@@ -42,13 +26,21 @@
           all =
             builtins.map (
               jre: let
-                versionInfo = getJavaVersion jre;
-                version = builtins.readFile "${versionInfo}/version";
-                majorVersion = builtins.fromJSON (builtins.readFile "${versionInfo}/major");
+                version = lib.getVersion jre;
+                splitted = lib.splitString "." version;
+                candidate =
+                  if lib.elemAt splitted 0 == "1"
+                  then lib.elemAt splitted 1
+                  else lib.elemAt splitted 0;
+                majorVersion =
+                  if lib.match "([0-9]+)" candidate != null
+                  then lib.toInt candidate
+                  else if lib.match "8u.*" candidate != null
+                  then 8
+                  else throw "Invalid java version : ${candidate} of ${jre}";
               in {
-                path = "${jre}/bin/java";
-                version = version;
-                majorVersion = majorVersion;
+                path = lib.makeSearchPath "bin/java" [jre];
+                inherit version majorVersion;
                 valid = true;
               }
             )
